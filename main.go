875255d9aae793144a26b323defdb1b4d5b01505
NKRifat47 <nk.rifat47@gmail.com>
2026-06-08 10:03:02 +0600

    update

--- main.go
+++ main.go
@@ -8,18 +8,4 @@ import (
 func main() {
 	cmd.Serve()
 
-	// jwt, err := util.CreateJwt("my-sercet", util.Payload{
-	// 	Sub: 47,
-	// 	FirstName: "Rifat",
-	// 	LastName: "Kobir",
-	// 	Email:     "rifat@test.com",
-	// 	IsSopOwner: false,
-	// })
-
-	// if err != nil{
-	// 	fmt.Println("This is the error", err)
-	// 	return
-	// }
-
-	// fmt.Println(jwt)
 }
