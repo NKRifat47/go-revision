6c6940d9c26522d0b4d699a34891af538fd68235
NKRifat47 <nk.rifat47@gmail.com>
2026-06-08 08:44:08 +0600

    go class upto 056 full complete

--- rest/handlers/create_product.go
+++ rest/handlers/create_product.go
@@ -1,64 +1,15 @@
 package handlers
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"ecommerce/config"
 	"ecommerce/database"
 	"ecommerce/util"
 	"encoding/base64"
 	"encoding/json"
 	"net/http"
-	"strings"
 )
 
 func CreateProduct(w http.ResponseWriter, r *http.Request) {
 	
-	header:= r.Header.Get("Authorization")
-
-	if header == "" {
-		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
-		return
-	}
-
-	headerArr := strings.Split(header, " ")
-
-	if len(headerArr) != 2 {
-		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
-		return
-	}
-	
-	accessToken := headerArr[1]
-
-	tokenParts := strings.Split(accessToken, ".")
-
-	if len(tokenParts) != 3 {
-		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
-		return
-	}
-
-	jwtHeader := tokenParts[0]
-	jwtPayload := tokenParts[1]
-	jwtSignature := tokenParts[2]
-
-	message := jwtHeader + "." + jwtPayload
-
-	cnf := config.GetConfig()
-
-	byteArrSecret := []byte(cnf.JwtSecretKey)
-	byteArrMessage := []byte(message)
-	
-	h := hmac.New(sha256.New, byteArrSecret)
-	h.Write(byteArrMessage)
-
-	hash := h.Sum(nil)
-	newSigneture := base64UrlEncode(hash)
-
-	if newSigneture != jwtSignature{
-		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
-		return
-	}
-
 	var newProduct database.Product
 
 	decoder := json.NewDecoder(r.Body)
---
+++ rest/middleware/authenticate_jwt.go
@@ -0,0 +1,65 @@
+package middleware
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"ecommerce/config"
+	"ecommerce/util"
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
+
+func AuthenticateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header:= r.Header.Get("Authorization")
+
+	if header == "" {
+		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	headerArr := strings.Split(header, " ")
+
+	if len(headerArr) != 2 {
+		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	
+	accessToken := headerArr[1]
+
+	tokenParts := strings.Split(accessToken, ".")
+
+	if len(tokenParts) != 3 {
+		util.SendError(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	jwtHeader := tokenParts[0]
+	jwtPayload := tokenParts[1]
+	jwtSignature := tokenParts[2]
+
+	message := jwtHeader + "." + jwtPayload
+
+	cnf := config.GetConfig()
+
+	byteArrSecret := []byte(cnf.JwtSecretKey)
+	byteArrMessage := []byte(message)
+	
+	h := hmac.New(sha256.New, byteArrSecret)
+	h.Write(byteArrMessage)
+
+	hash := h.Sum(nil)
+	newSigneture := base64UrlEncode(hash)
+
+	if newSigneture != jwtSignature{
+		util.SendError(w, http.StatusUnauthorized, "Unauthorized give me valid token")
+		return
+	}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func base64UrlEncode(data []byte) string {
+	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(data)
+}
